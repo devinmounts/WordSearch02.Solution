deb043302a2b9fd0e2d19cefab3b6274de0737d8
Devin Mounts <devinmounts@gmail.com>
2018-07-06 09:47:44 -0700

    alter namespaces oncontrollers to allow successful build.

--- WordSearch02/Controllers/WordCounterController.cs
+++ WordSearch02/Controllers/WordCounterController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WordSearch02.Models;
+using WordSearch02.Controllers;
 
 namespace WordSearch02.Controllers
 {
--- WordSearch02Tests/ControllerTests/WordCounterControllerTests.cs
+++ WordSearch02Tests/ControllerTests/WordCounterControllerTests.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using WordSearch02.Controllers;
+using WordSearch02.Models;
+
 namespace WordSearch02Tests.ControllerTests
 {
+    [TestClass]
     public class WordCounterControllerTests
     {
-        public WordCounterControllerTests()
+        [TestMethod]
+        public void WordCounterForm_ReturnsCorrectView_True()
         {
+            WordCounterController controller = new WordCounterController();
+            WordCounterController wordCounterForm = controller.WordCounterForm();
+            Assert.IsInstanceOfType(wordCounterForm, typeof(ViewResult));
         }
     }
 }
